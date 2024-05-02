dc6256e25ebf412cadc7c5cfc41f2353480561b0
Mantra Yang <myang@udel.edu>
2024-05-02 12:23:52 -0400

    Questions are now displaying one at a time.

--- src/Pages/BQPage.tsx
+++ src/Pages/BQPage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 //import React, { useState } from "react";
 //import { Button } from "react-bootstrap";
+import { useState } from "react";
 import "./BQPage.css";
 //import axios from "axios";
 
@@ -15,161 +16,185 @@ interface Question {
   question: string;
   answers:string[] | null;
   type: "short answer" | "multiple choice";
+  selectedAnswer: string | null;
 }
 
 
 function BQPage({ setPage }: Props): JSX.Element {
 
-    // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    // const [userSelection, setUserSelection] = useState < string | null > (null);
   
     const questions1: Question [] = [
       //25 questions total
       {
         question: "Would you prefer working from home, in an office / on site, or hybrid? (pick one)",
         answers: ["Working from home", "In an office / on site", "Hybrid"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       },
       {
         question: "What salary would you not feel comfortable earning less than? (pick one)",
         answers:["45K", "65K", "85K", "105K", "125K", "145K", "165K", "200K"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Do you prefer to do work individually, in a small group (2-4 people), or a team (more than 4 people)?",
         answers: ["Individually", "Small group (2-4 people)", "Team (more than 4 people)"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "How would you describe your ideal work environment in one word?",
         answers: null,
-        type:"short answer"
+        type:"short answer",
+        selectedAnswer: null
       }
       ,
       {
         question: "How can you describe yourself in one word?",
         answers: null,
-        type: "short answer"
+        type: "short answer",
+        selectedAnswer: null
       }
       ,
       {
         question: "What subject are you the best at?",
         answers: ["english", "math", "science", "physical activity"],
         type:"multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "How would you rate your ability to learn new skills? (pick one)",
         answers: ["Excellent", "Good", "Average", "Below average", "Poor"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Please select your response to the following statement: I work well in fast paced environments",
         answers: ["Yes", "No"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Would you enjoy traveling for work?",
         answers: ["Yes", "No"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "What is the maximum amount of hours you would prefer to work?",
         answers: ["15 Hrs", "30 Hrs", "40 Hrs", "60 Hrs", "As many hours as needed"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "What is your ideal shift time?",
         answers: ["7am - 3pm", "9am - 5pm", "11am-4pm", "4pm-10pm", "11pm-6am", "I want to work when I want."],
-        type:"multiple choice"
+        type:"multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "List three activities you enjoy:",
         answers: null,
-        type: "short answer"
+        type: "short answer",
+        selectedAnswer: null
       },
       {
         question: "Would you prefer to be relatively sedentary or active at work?",
         answers: ["Sedentary", "Active"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       },
       {
         question: "Would you prefer to dress formally at work or casually?",
         answers: ["Formal", "Casual"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "What location would you prefer to live in?",
         answers: ["City", "Suburban", "Coastal/Beach", "Urban"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "What kind of learner are you?",
         answers: ["Visual", "Auditory", "Read/Write", "Kindaesthetic"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Favorite activity? (one word answer)",
         answers: null,
-        type: "short answer"
+        type: "short answer",
+        selectedAnswer: null
       }
       ,
       {
         question: "What is your dreamjob? (one to two word answer)",
         answers: null,
-        type: "short answer"
+        type: "short answer",
+        selectedAnswer: null
       }
       ,
       {
         question: "How do you prefer to spend your workday?",
         answers: ["Solving problems","Meeting with colleagues","Creating new ideas","Following established procedures"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Please select your level of agreement with the following statement: I prefer working independently rather than with a team.",
         answers: ["True", "False"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       },
 
       {
         question: "Do you enjoy helping others?",
         answers: ["Yes", "No"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Please select your level of agreement with the following statement: I prefer jobs that require a lot of attention to detail",
         answers: ["True", "False"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Do you enjoy working in high-pressure situations?",
         answers: ["Yes", "No"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "Please select your level of agreement with the following statement: I prefer consistent work hours over a flexible schedule.",
         answers: ["True", "False"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
       ,
       {
         question: "How do you approach decision-making?",
         answers: ["Analyze data and facts","Trust your intuition","Consult with others for advice","Take time to weigh all options"],
-        type: "multiple choice"
+        type: "multiple choice",
+        selectedAnswer: null
       }
     ]
   
@@ -215,7 +240,21 @@ function BQPage({ setPage }: Props): JSX.Element {
   //   }
   // }
 
-  // const currentQ = questions1[currentQuestionIndex];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const questions2 = [...questions1];
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+
+  const nextQuestion = () => {
+    if (currentQuestionIndex < questions2.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+  
+  const previousQuestion = () => {
+    if (currentQuestionIndex < questions2.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
 
   return (
     <div className="Bbody">
@@ -236,19 +275,20 @@ function BQPage({ setPage }: Props): JSX.Element {
       <br></br>
       <div style={{ height: "300px" }}></div>
       <div>
-        {questions1.map((question) => (
-            <div key={question.question} className="question-container">
-        <h2>{question.question}</h2>
-        {question?.type === "multiple choice" ? (
+        <h2>Question {currentQuestionIndex +1} of {questions2.length}</h2>
+        <div key= {questions2[currentQuestionIndex].question} className = "question-con">
+        <h2>{questions2[currentQuestionIndex].question}</h2>
+        {questions2[currentQuestionIndex].type === "multiple choice" ? (
           <ul>
-            {question.answers?.map((answer) => (
+            {questions2[currentQuestionIndex].answers?.map((answer) => (
               <li key={answer}>
               <input
                 type="radio"
                 id={answer} 
-                name="answer" 
+                name={`question-$.id}`}
                 value={answer}
-                //checked={userSelection === answer} 
+                required={true}
+                //checked = {selectedAnswers[currentQuestionIndex as string] === answer}
                 //onChange={handleAnswerChange}
               />
               <label htmlFor = {answer}>{answer}</label>
@@ -263,12 +303,12 @@ function BQPage({ setPage }: Props): JSX.Element {
         />
         )}
         </div>
-        ))}
-        {/* <button disabled={!currentQuestion} onClick={handleNextQuestion}>
-          Next Question
+        <button onClick = {previousQuestion} disabled = {currentQuestionIndex === 0}>
+          Previous Question
         </button>
-      </div>
-    ); */}
+        <button onClick={nextQuestion} disabled={currentQuestionIndex === questions2.length - 1}>
+      Next
+    </button>
     </div>
   </div>
   );
