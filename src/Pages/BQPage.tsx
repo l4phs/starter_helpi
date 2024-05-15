a319af7f59b0a9bb5e94a2937a367781a83817be
Mantra Yang <myang@udel.edu>
2024-05-15 19:19:42 -0400

    Updated instructions for the bq page.

--- src/Pages/BQPage.tsx
+++ src/Pages/BQPage.tsx
@@ -264,7 +264,9 @@ function BQPage(props: Props): JSX.Element {
     <div className="BQPage">
       <div className="Background"></div>
       <h1 className="BQH">Basic Questions</h1> 
-      <h3 className = "Description"> Welcome to the Basic Quiz!  </h3>
+      <h3 className = "Description"> Welcome to the Basic Quiz! There are 18 total questions but you can answer as many
+      or as few as you would like! The more questions you answer, the more accurate your results will be! You will be able to review
+      your answers and go back and change any of them before you submit your results. </h3>
       {submitted ? (
         // Display submitted answers if submitted is true
         <div className="SubmittedAnswers">
